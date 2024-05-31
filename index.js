094c2f80e833692fb07b1b2b817db1e47dc97cca
Pedro Euzebio <pedroeuzebio.contato@gmail.com>
2024-05-31 20:27:23 -0300

    feat: alter profile image when light and dark mode altern

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ function toggleMode() {
   const html = document.documentElement
   html.classList.toggle("light")
 
+  const img = document.querySelector("#profile img")
+
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "assets/avatar-light.png")
+  } else {
+    img.setAttribute("src", "assets/avatar.png")
+  }
+
   // if (html.classList.contains("light")) {
   //   html.classList.remove("light")
   // } else {
