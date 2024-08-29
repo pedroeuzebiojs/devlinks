2ee80277113ef36e0f984cf2de08bd6f0fb7a57a
Pedro Euzebio <pedroeuzebio.contato@gmail.com>
2024-08-29 19:40:13 -0300

    atualiza código js

--- index.js
+++ index.js
@@ -1,26 +1,8 @@
+const switchButton = document.querySelector("#switch-button");
+
 function toggleMode() {
-  const html = document.documentElement
-  html.classList.toggle("light")
-
-  const img = document.querySelector("#profile img")
-
-  if (html.classList.contains("light")) {
-    img.setAttribute("src", "assets/avatar-light.png")
-    img.setAttribute(
-      "alt",
-      "Foto do Pedro Euzebio sorrindo com camisa social azul clara na praia e no fundo o mar aberto."
-    )
-  } else {
-    img.setAttribute("src", "assets/avatar.png")
-    img.setAttribute(
-      "alt",
-      "Foto do Pedro Euzebio sorrindo com camisa social azul clara."
-    )
-  }
-
-  // if (html.classList.contains("light")) {
-  //   html.classList.remove("light")
-  // } else {
-  //   html.classList.add("light")
-  // }
+  const html = document.documentElement;
+  html.classList.toggle("light");
 }
+
+switchButton.addEventListener("click", toggleMode);
